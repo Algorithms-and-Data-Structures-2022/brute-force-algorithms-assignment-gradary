e19e493da9aa7de8bd017394eb474092d74f224c
gradary <100384474+gradary@users.noreply.github.com>
2022-09-11 01:38:51 +0300

    2 task

--- src/bits.cpp
+++ src/bits.cpp
@@ -1,17 +1,18 @@
 #include "assignment/bits.hpp"
 
-#include <cassert>  // assert
+#include <cassert>
 
 namespace assignment {
 
   bool is_bit_set(int mask, int pos) {
     assert(mask >= 0 && pos >= 0 && pos < 30);
-    return false;
+    return (mask &(1 << pos));
   }
+
 
   int set_bit(int mask, int pos) {
     assert(mask >= 0 && pos >= 0 && pos < 30);
-    return 0;
+    return mask | (1 << pos);
   }
 
   std::vector<int> mask2indices(const std::vector<int>& elems, int mask) {
@@ -34,14 +35,14 @@ namespace assignment {
 
     std::vector<int> masked_elems;
 
-    for (int pos = 0; pos < static_cast<int>(elems.size()); pos++) {
+    for (int idx = 0; idx < static_cast<int>(elems.size()); idx++) {
 
-      if (is_bit_set(mask, pos)) {
-        masked_elems.push_back(elems[pos]);
+      if (is_bit_set(mask, idx)) {
+        masked_elems.push_back(elems[idx]);
       }
     }
 
     return masked_elems;
   }
 
-}  // namespace assignment
+}
--- src/knapsack/backtracking.cpp
+++ src/knapsack/backtracking.cpp
@@ -9,12 +9,9 @@ namespace assignment {
   std::vector<int> KnapsackBacktracking::solve(const Profits& profits, const Weights& weights, int capacity) const {
     assert(profits.size() == weights.size() && capacity > 0);
 
-    // результат: наибольшая "польза"
     int best_profit = 0;
     int best_profit_mask = 0;
 
-    // вызов вспомогательного метода: обратите внимание на входные аргументы
-    // и на то, как они передаются (по значению или ссылке, почему так?)
     solve(profits, weights, capacity, -1, 0, 0, 0, best_profit, best_profit_mask);
 
     return mask2indices(profits, best_profit_mask);
@@ -23,25 +20,22 @@ namespace assignment {
   void KnapsackBacktracking::solve(const Profits& profits, const Weights& weights, int capacity, int index, int mask,
                                    int weight, int profit, int& best_profit, int& best_profit_mask) const {
 
-    // Ограничение 0: выход за пределы
-    if (index == static_cast<int>(profits.size())) {
-      return;
-    }
+    if (index == static_cast<int>(profits.size())) {return;}
 
-    // Ограничение 1: превышение лимита емкости рюкзака
-    if (weight > capacity) {
-      return;
-    }
+    if (weight > capacity) {return;}
 
-    // ... если текущая "польза" максимальна, обновляем наилучшую "пользу"
     if (profit > best_profit) {
-      // ...
+      best_profit = profit;
+      best_profit_mask = mask;
+
+
+
     }
 
-    // рассматриваем следующий элемент
     index += 1;
 
-    // ... рекурсивные вызовы со включением/исключением следующего элемента
+    solve(profits, weights, capacity, index, set_bit(mask, index), weight+weights[index], profit+profits[index], best_profit, best_profit_mask);
+    solve(profits, weights, capacity, index, mask, weight, profit, best_profit, best_profit_mask);
   }
 
 }  // namespace assignment
